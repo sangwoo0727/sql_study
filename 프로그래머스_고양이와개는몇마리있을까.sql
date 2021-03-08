8bef6cb6204ce11083bcfa367efa956f8aa40413
sangwoo0727 <kangsw9395@gmail.com>
2021-03-08 15:59:37 +0900

    프로그래머스 고양이와 개는 몇 마리있을까?

--- 프로그래머스_고양이와개는몇마리있을까.sql
+++ 프로그래머스_고양이와개는몇마리있을까.sql
@@ -1,3 +1,9 @@
+-- 테이블에서 동물의 종류가 고양이와 개인 동물의 각각 개수를 출력하는 문제이다.
+-- 고양이가 먼저 나오도록 출력한다.
+-- Where 절을 이용해서 Cat과 Dog만 나오도록 먼저 조건을 걸고,
+-- ANIMAL_TYPE으로 GROUP BY를 시킨다.
+-- 이후 정렬을 하고 출력하였다.
+
 SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) as count
 FROM ANIMAL_INS
 WHERE ANIMAL_TYPE = 'Cat' or ANIMAL_TYPE = 'Dog'
